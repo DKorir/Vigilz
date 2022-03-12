a4184a7d404772151bff513e1846feb11590bb20
DKorir <dominic.korir@student.moringaschool.com>
2022-03-12 17:22:39 +0300

    create post model for the project with imagefield, title,user,post date and the url for the project

--- mypro/models.py
+++ mypro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    photo = models.ImageField(upload_to='images')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    post_date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.title
