6ff39e49efd1ed3fa0984abcebec1c79c423854b
DKorir <dominic.korir@student.moringaschool.com>
2022-03-12 18:05:29 +0300

    add methods for the post model for delete,save,search

--- mypro/models.py
+++ mypro/models.py
@@ -18,4 +18,14 @@ class Post(models.Model):
     post_date = models.DateField(auto_now_add=True)
     def __str__(self):
         return self.title
+    def delete_post(self):
+        self.delete()
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+    def save_post(self):
+        self.save()
 
