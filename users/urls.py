ec2a2af58148dbfc37c8ebb318d6caab6e9c951e
DKorir <dominic.korir@student.moringaschool.com>
2022-03-13 17:44:47 +0300

    add password page url

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('password/',PasswordsChangeView.as_view(template_name='registration/change-password.html')),
 ]
