ed068807465fe3a4d9463109e8b35ff61c38659d
DKorir <dominic.korir@student.moringaschool.com>
2022-03-13 17:19:52 +0300

    create forms file in users app

---
+++ users/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.models import User
+from django import forms
+from mypro.models import Profile
+
+class ProfilePageForm(forms.ModelForm):
+    class Meta:
+        model = Profile
