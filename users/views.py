6414d3fc0a34715ae23e5358e31968baab452062
DKorir <dominic.korir@student.moringaschool.com>
2022-03-13 17:37:37 +0300

    create show profile page view

--- users/views.py
+++ users/views.py
@@ -15,3 +15,19 @@ class CreateProfilePageView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio','profile_pic','contact']
+    success_url = reverse_lazy('home')
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+    def get_context_data(self, *args, **kwargs):
+        # users = Profile.objects.all()
+        context = super(ShowProfilePageView,self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
