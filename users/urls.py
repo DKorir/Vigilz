2cdb128fc77f3e26465dd1b90b194fc45426953e
DKorir <dominic.korir@student.moringaschool.com>
2022-03-13 17:43:22 +0300

    add urls for register

--- users/urls.py
+++ users/urls.py
@@ -2,3 +2,7 @@ from django.urls import path
 from .views import ShowProfilePageView, UserRegisterView, UserEditView, PasswordsChangeView, EditProfilePageView, CreateProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
+
+urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='register'),
+]
